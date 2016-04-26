c010a3c107697a163fa1d5b470940c85d888b92d
Karena Nguyen <knguyen63@mail.usf.edu>
2016-04-26 14:20:46 -0400

    Adding the first function

---
+++ R/ConstantF.R
@@ -0,0 +1,15 @@
+#' @title constant.f
+#' @param m death rate of uninfected and pre-patent snails, a numeric number or vector
+#' @param t pre-patent period, a numeric number or vector
+#' @return a numeric vector, the resulting constant for the main equation
+
+constant.f <- function(m,t){
+  
+  constant <- exp(-m*t)
+  return(constant)
+  
+}
+
+#' @references Anderson, R. M. a. R. M. M. 1979. 
+#' Prevalence of schistosome infections within molluscan populations: observed patterns and theoretical predictions. 
+#' Parasitology 79:63-94.
